4671c76a82c9efc60910a9d2444b9edc4e743236
half-programmer <Kingxc96@outlook.com>
2016-10-15 00:18:17 +0800

    增加网页静态代码

--- main.py
+++ main.py
@@ -53,12 +53,11 @@ from RegistandLogin.WloginHandler import WLoginHandler
 define("port", default=800, help="run on the given port", type=int)
 
 
-<<<<<<< HEAD
+
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("index.html")
-=======
->>>>>>> f46e2017865784789db852994a9a3e9dd36bdf6d
+        self.render(r"static/index.html")
+
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -79,7 +78,7 @@ class Application(tornado.web.Application):
              (r"/weixin/login", WLoginHandler),
              (r"/weixin/activity/registerlist",WAcseeregist)
         ]
-        tornado.web.Application.__init__(self, handlers)
+        tornado.web.Application.__init__(self, handlers, static_path)
 
         self.db = scoped_session(sessionmaker(bind=engine,
                                               autocommit=False, autoflush=True,
