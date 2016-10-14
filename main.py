dc3e65680cf8a04bf62db29ecf885603b2cc01eb
half-programmer <Kingxc96@outlook.com>
2016-10-15 01:30:17 +0800

    尝试解决静态问题

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ define("port", default=800, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render(r"static/index.html")
+        self.render(r"index.html")
 
 
 class Application(tornado.web.Application):
@@ -77,13 +77,14 @@ class Application(tornado.web.Application):
              (r"/weixin/login", WLoginHandler),
              (r"/weixin/activity/registerlist",WAcseeregist)
         ]
-        tornado.web.Application.__init__(self, handlers, static_path)
+        tornado.web.Application.__init__(self, handlers,static_path,debug=True)
 
         self.db = scoped_session(sessionmaker(bind=engine,
                                               autocommit=False, autoflush=True,
                                               expire_on_commit=False))
 
 # session负责执行内存中的对象和数据库表之间的同步工作 Session类有很多参数,使用sessionmaker是为了简化这个过程
+#template_path = os.path.join(os.path.dirname(__file__), "temploop")
 static_path = os.path.join(os.path.dirname(__file__), r"static")
 scripts_path = os.path.join(os.path.dirname(__file__), r"scripts")
 
