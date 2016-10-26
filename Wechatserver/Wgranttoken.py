ae9268ef2eadfad96fc07e804447467b5f072aba
half-programmer <Kingxc96@outlook.com>
2016-10-26 18:44:05 +0800

    添加证书

--- Wechatserver/Wgranttoken.py
+++ Wechatserver/Wgranttoken.py
@@ -2,11 +2,13 @@
 '''
 刷新微信access_token
 '''
+import requests
 
 from Wconf import Wconf
 conf = Wconf.conf
 def GetActoken():
-
+    requests.get('https://api.weixin.qq.com/cgi-bin/token', verify=True)
+    requests.get('https://api.weixin.qq.com/cgi-bin/ticket/getticket', verify=True)
     conf.grant_access_token()
     conf.grant_jsapi_ticket()
 
