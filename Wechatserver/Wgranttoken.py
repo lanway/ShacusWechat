fc4ceedaac5cbf320211763dbba7e01143e33948
half-programmer <Kingxc96@outlook.com>
2016-10-26 19:33:47 +0800

    添加证书

--- Wechatserver/Wgranttoken.py
+++ Wechatserver/Wgranttoken.py
@@ -2,13 +2,20 @@
 '''
 刷新微信access_token
 '''
-import requests
+import urllib
 
+import requests
+import ssl
 from Wconf import Wconf
 conf = Wconf.conf
 def GetActoken():
+    context = ssl._create_unverified_context()
+    urllib.urlopen("https://api.weixin.qq.com/cgi-bin/token", context=context)
+    urllib.urlopen("https://api.weixin.qq.com/cgi-bin/ticket/getticket", context=context)
     requests.get('https://api.weixin.qq.com/cgi-bin/token', verify=True)
     requests.get('https://api.weixin.qq.com/cgi-bin/ticket/getticket', verify=True)
+    # This restores the same behavior as before.
+
     conf.grant_access_token()
     conf.grant_jsapi_ticket()
 
