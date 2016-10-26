e95952ad193eeb6cacf7075823016137f3b5a0cb
half-programmer <Kingxc96@outlook.com>
2016-10-26 22:49:37 +0800

    增加个人主页及评论

--- Userinfo/WUserhomepager.py
+++ Userinfo/WUserhomepager.py
@@ -7,7 +7,7 @@
 import json
 
 from BaseHandlerh import BaseHandler
-from Database.tables import User, WApInfo
+from Database.tables import User, WApInfo, WAppointment
 
 
 class UHandler(BaseHandler):
@@ -37,24 +37,29 @@ class UHandler(BaseHandler):
                             comment_content = each.WAImcomment
                             score = each.WAIpscore  # 摄影师获得的评分
                             comment_user_id = each.WAImid  # 模特的id
+                            apid = each.WAIappoid
+
                             try:
                                 model = self.db.query(User).filter(User.Uid == comment_user_id).one()
+                                appointment = self.db.query(WAppointment).filter(WAppointment.WAPid == apid ).one()
+                                ap_name = appointment.WAPtitle
                                 model_name = model.Ualais
                                 comment_entry = dict(
                                     comment=comment_content,
                                     alais=model_name,
-                                    score=score
+                                    score=score,
+                                    title=ap_name
                                 )
                                 comments.append(comment_entry)
                                 self.retjson['code'] = '200'
                                 self.retjson['contents'] = u"成功"
                             except Exception, e:
-                                self.retjson['code'] = ''
+                                self.retjson['code'] = u'40005'
                                 self.retjson['contents'] = u"获取评论用户出错"
-
-
                 except Exception, e:
                     print e
+                self.retjson['code'] = u'40002'
+                self.retjson['contents'] = u"该用户作为摄影师没有发布过约拍"
                 try:
                     # 用户作为模特参加的约拍
                     asmodels = self.db.query(WApInfo).filter(WApInfo.WAImid == uid).all()
@@ -80,10 +85,14 @@ class UHandler(BaseHandler):
                                 self.retjson['contents'] = u"获取评论用户出错"
                 except Exception, e:
                     print e
+                    self.retjson['code'] = u'40003'
+                    self.retjson['contents'] = u"该用户作为模特没有发布过约拍"
                 if comments:
                     self.retjson['comments'] = comments
             except Exception, e:
                 print e
+            self.retjson['code'] = u'40001'
+            self.retjson['contents'] = u"该用户不存在"
         self.write(json.dumps(self.retjson, ensure_ascii=False, indent=2))
 
 
