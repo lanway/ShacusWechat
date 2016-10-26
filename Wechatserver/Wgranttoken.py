e36ec0079ff7c81a551eb6f6383159f68f36dff5
lanway <115564098@qq.com>
2016-10-26 11:48:26 +0800

    lalala

--- Wechatserver/Wgranttoken.py
+++ Wechatserver/Wgranttoken.py
@@ -8,7 +8,7 @@ import ssl
 from Wconf import Wconf
 conf = Wconf.conf
 def GetActoken():
-    ssl._create_default_https_context = ssl._create_unverified_context
+
     conf.grant_access_token()
     conf.grant_jsapi_ticket()
 
